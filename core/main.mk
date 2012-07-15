98e4722b261e7b7282f7ece8e05ab946a31dadfb
rascarlo <rascarlo@gmail.com>
2012-07-15 11:33:29 +0200

    Debugging TRUE

--- core/main.mk
+++ core/main.mk
@@ -254,7 +254,7 @@ ifneq (,$(user_variant))
     ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.lockprof.threshold=500
   else
     # Disable debugging in plain user builds.
-    enable_target_debugging :=
+    enable_target_debugging := true
   endif
 
   # Don't odex for user 
